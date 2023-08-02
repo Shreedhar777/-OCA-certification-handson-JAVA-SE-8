47e13375d01fe7698c79484f308cc36b9a92ae26
Shreedhar Gouli <71750336+Shreedhar777@users.noreply.github.com>
2023-08-02 19:28:12 +0530

    Add files via upload

---
+++ Exercise/src/Test.java
@@ -0,0 +1,11 @@
+ 
+public class Test {
+     static Boolean[] arr = new Boolean[1];
+     public static void main(String[] args) {
+         if(arr[0]) {
+             System.out.println(true);
+         } else {
+             System.out.println(false);
+         }
+     }
+}
